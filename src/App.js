85e828f3b6e035ba5adab479fe1e5a60e9b93d3e
markscodingexperience <markchristanp@gmail.com>
2024-07-19 21:12:58 +0800

    Creating first component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/Header';
 
 function App() {
+  const name = "Mark"
+  // const client_id = "801979867548-nbt0usbqnh11u0kqbm6detd2vdhh07rd.apps.googleusercontent.com";
+  // const client_secret = "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";
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <>
+            <h2>Your Notes</h2>
+            <button> + </button>
+            <select name="" id="">Personal</select>
+            <button type="button">#all</button>
+        </>
+       
+      );
+}
+ 
+
+export default Header;
