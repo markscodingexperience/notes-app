52bf6fab9578ef7561bd660e48234bf3c2faf7c7
markscodingexperience <markchristanp@gmail.com>
2024-08-10 22:52:41 +0800

    final

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import { useState } from "react";
 
 
 function App() {
-  //const name = "Mark"
-  // const client_id = "801979867548-nbt0usbqnh11u0kqbm6detd2vdhh07rd.apps.googleusercontent.com";
-  // const client_secret = "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";
+
   const [list, setList] = useState([]);
 
   const [filter, setFilter] = useState("");
